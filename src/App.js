d2e72754589b69e16bf256e29aeba671018957bc
ijeoma <ijeomaodinko@gmail.com>
2022-08-11 07:15:20 -0400

    fix linters erroron eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 // import React from 'react';
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 import './components/Calculator.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (<Calculator />);
   }
