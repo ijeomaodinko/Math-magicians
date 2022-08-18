50b45b65c12b2edba3889dfa92ca9de4908a9ba9
ijeoma <ijeomaodinko@gmail.com>
2022-08-18 11:14:31 -0400

    Add files for single page application

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,55 +1,74 @@
-import React, { useState } from 'react';
+import React from 'react';
 import calculate from '../logic/calculate';
 
-function Calculator() {
-  const [state, setState] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.clickHandle = this.clickHandle.bind(this);
+  }
 
-  const clickHandle = (event) => {
-    setState((prevState) => calculate(prevState, event.target.textContent));
+  clickHandle = (event) => {
+    const calc = calculate(this.state, event.target.textContent);
+    this.setState(calc);
   };
 
-  return (
-    <div className="wrapper">
-      <div className="screen">
-        <span className="input">{state.total}</span>
-        <span className="input">{state.operation}</span>
-        <span className="input">{state.next}</span>
+  render() {
+    const {
+      total,
+      next,
+      operation,
+    } = this.state;
+
+    return (
+      <div className="imenu">
+        <div>
+          <h2 className="menup">Let&apos;s do some Math</h2>
+        </div>
+        <div className="wrapper">
+          <div className="screen">
+            <span className="input">{total}</span>
+            <span className="input">{operation}</span>
+            <span className="input">{next}</span>
+          </div>
+          <div className="button-box">
+            <button type="button" onClick={this.clickHandle} className="allClear">AC</button>
+            <button type="button" onClick={this.clickHandle} className="plusMinus">+/-</button>
+            <button type="button" onClick={this.clickHandle} className="percent">%</button>
+            <button type="button" onClick={this.clickHandle} className="divide last">÷</button>
+            <br />
+
+            <button type="button" onClick={this.clickHandle} className="seven">7</button>
+            <button type="button" onClick={this.clickHandle} className="eight">8</button>
+            <button type="button" onClick={this.clickHandle} className="nine">9</button>
+            <button type="button" onClick={this.clickHandle} className="multiply last">x</button>
+            <br />
+
+            <button type="button" onClick={this.clickHandle} className="four">4</button>
+            <button type="button" onClick={this.clickHandle} className="five">5</button>
+            <button type="button" onClick={this.clickHandle} className="six">6</button>
+            <button type="button" onClick={this.clickHandle} className="subtract last">-</button>
+            <br />
+
+            <button type="button" onClick={this.clickHandle} className="one">1</button>
+            <button type="button" onClick={this.clickHandle} className="two">2</button>
+            <button type="button" onClick={this.clickHandle} className="three">3</button>
+            <button type="button" onClick={this.clickHandle} className="add  last">+</button>
+            <br />
+
+            <button type="button" onClick={this.clickHandle} className="zero">0</button>
+            <button type="button" onClick={this.clickHandle} className="point">.</button>
+            <button type="button" onClick={this.clickHandle} className="equalto  last">=</button>
+          </div>
+        </div>
       </div>
-      <div className="button-box">
-        <button type="button" onClick={clickHandle} className="allClear">AC</button>
-        <button type="button" onClick={clickHandle} className="plusMinus">+/-</button>
-        <button type="button" onClick={clickHandle} className="percent">%</button>
-        <button type="button" onClick={clickHandle} className="divide last">÷</button>
-        <br />
-
-        <button type="button" onClick={clickHandle} className="seven">7</button>
-        <button type="button" onClick={clickHandle} className="eight">8</button>
-        <button type="button" onClick={clickHandle} className="nine">9</button>
-        <button type="button" onClick={clickHandle} className="multiply last">x</button>
-        <br />
-
-        <button type="button" onClick={clickHandle} className="four">4</button>
-        <button type="button" onClick={clickHandle} className="five">5</button>
-        <button type="button" onClick={clickHandle} className="six">6</button>
-        <button type="button" onClick={clickHandle} className="subtract last">-</button>
-        <br />
-
-        <button type="button" onClick={clickHandle} className="one">1</button>
-        <button type="button" onClick={clickHandle} className="two">2</button>
-        <button type="button" onClick={clickHandle} className="three">3</button>
-        <button type="button" onClick={clickHandle} className="add  last">+</button>
-        <br />
-
-        <button type="button" onClick={clickHandle} className="zero">0</button>
-        <button type="button" onClick={clickHandle} className="point">.</button>
-        <button type="button" onClick={clickHandle} className="equalto  last">=</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Calculator;
