802ae8c0bfa40df8ddd2f61aac5db0e532fd81a2
ijeoma <ijeomaodinko@gmail.com>
2022-08-18 19:44:59 -0400

    Add test files

---
+++ src/__test__/operate.test.js
@@ -0,0 +1,22 @@
+import operate from '../logic/operate';
+
+
+test('to test for addition operation', () => {
+expect(operate(1, 2,'+')).toEqual('3');
+});
+
+test ('test for subtraction operation', () => {
+    expect(operate(5,4,'-')).toEqual('1');
+});
+
+test ('to test for division operation', () => {
+    expect(operate(6,1,'÷')).toEqual('6');
+});
+
+test('test for multiplication operation', () =>{
+    expect(operate(2,5,'x')).toEqual('10');
+});
+
+test('to test for modulus operation', () => {
+    expect(operate(5,2,'%')).toEqual('1');
+});
