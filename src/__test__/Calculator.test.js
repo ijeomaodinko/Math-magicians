b25bdc67f4d16d295bacad14458aa3146396c898
ijeoma <ijeomaodinko@gmail.com>
2022-08-18 11:37:39 -0400

    Add linters

--- src/__test__/Calculator.test.js
+++ src/__test__/Calculator.test.js
@@ -2,9 +2,8 @@ import { render, screen } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 import '@testing-library/jest-dom';
 
-
 test('Display text on the screen', () => {
-    render(<Calculator />);
-    const element = screen.getByText(/Let's do some Math/i);
-    expect(element).toBeInTheDocument();
-  });
+  render(<Calculator />);
+  const element = screen.getByText(/Let's do some Math/i);
+  expect(element).toBeInTheDocument();
+});
